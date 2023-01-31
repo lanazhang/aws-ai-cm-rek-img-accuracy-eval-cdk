d50874c7adc0bfde5801e69c9b4ffe430e4fd0a6
Lana Zhang <lanaz@amazon.com>
2023-01-31 03:39:13 +0000

    Add readme file

--- app.py
+++ app.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
-import os
-import uuid
-import json
 import aws_cdk as cdk
 
+from aws_cdk import Stack,CfnOutput
+import uuid
 from accuracy_eval.backend_provision import BackendProvision
-from accuracy_eval.a2i_provision import A2iProvision
 from accuracy_eval.frontend_provision import FrontendProvision
+from accuracy_eval.a2i_provision import A2iProvision
 
-instance_hash = str(uuid.uuid4())[0:5]
+class RootProvision(Stack):
+    instance_hash = None
+    def __init__(self, scope):
+        super().__init__(scope, "cm-accuracy-eval-RootStack", description="AWS Content Moderation accuracy evaluation provision stack. Beta")
+    
+        self.instance_hash = str(uuid.uuid4())[0:5]
+        
+        a2i_stack = A2iProvision(self, "A2iProvisionStack", description="AWS Content Moderation accuracy evaluation - A2I deployment statck.",
+            instance_hash_code=self.instance_hash
+        )
+        
+        backend_stack = BackendProvision(self, "BackendProvisionStack", description="AWS Content Moderation accuracy evaluation - Backend deployment statck.",
+            instance_hash_code=self.instance_hash,
+            cognito_user_pool_id = a2i_stack.ouput_cognito_user_pool_id
+        )
+    
+        frontend_stack = FrontendProvision(self, "FrontProvisionStack", description="AWS Content Moderation accuracy evaluation - Frontend deployment statck.",
+            instance_hash_code=self.instance_hash,
+            api_gw_base_url = backend_stack.api_gw_base_url,
+        )
 
+        CfnOutput(self, "Website URL",
+            value=f"https://{frontend_stack.output_url}"
+        )
+        
 app = cdk.App()
 
-a2i_stack = A2iProvision(app, "A2iProvisionStack",
-    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    instance_hash_code=instance_hash
-)
+root_stack = RootProvision(app)
 
-backend_stack = BackendProvision(app, "BackendProvisionStack",
-    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    instance_hash_code=instance_hash,
-    cognito_user_pool_id = a2i_stack.ouput_cognito_user_pool_id
-)
-
-frontend_stack = FrontendProvision(app, "FrontProvisionStack",
-    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    instance_hash_code=instance_hash,
-    api_gw_base_url = backend_stack.api_gw_base_url,
-)
-
-app.synth()
